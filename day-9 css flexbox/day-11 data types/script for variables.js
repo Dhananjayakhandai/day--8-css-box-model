6e921c2bec510caa2f191f082b653c9ed3322e19
Dhananjayakhandai <dhananjayakhandai@gmail.com>
2025-07-23 07:53:57 +0530

    use of var, let keyword

---
+++ day-9 css flexbox/day-11 data types/script for variables.js
@@ -0,0 +1,62 @@
+let username = 'Akash'
+console.log(username)
+console.log(typeof username)
+
+let age = 25
+console.log(age)
+console.log(typeof age)
+
+let isHappy = true
+console.log(isHappy)
+console.log(typeof isHappy)
+
+let lastname = 'Singh'
+console.log(lastname)
+console.log(typeof lastname)
+
+let userIntro = "hi i am " + " username" + " " + "lastname" + age + "years old"
+console.log(userIntro)
+
+let a = undefined
+console.log(a)
+console.log(typeof a)
+
+let b;
+console.log(b)
+console.log(typeof b)
+
+b = false
+console.log(b, typeof b, 'xyz')
+
+let c = null
+console.log(c, typeof c)
+
+// <---------------------------------------------------------------------------------------->
+const hoursInDay = 24;
+// hoursInDay =20
+
+// const d;
+// =====================================================================
+var e
+e = 10;
+console.log(e)
+
+var f;
+console.log(f)
+
+{
+    var g = 20
+    let h = 30
+}
+
+// console.log(g)
+// console.log(h)
+
+
+// var - function scope
+// let - block scope
+
+console.log(y)    // undefined
+var y = 3020
+console.log(y)
+
