d05dd76c9d94805643e3c831115c0076d6d1809d
Dhananjayakhandai <dhananjayakhandai@gmail.com>
2025-07-24 08:08:23 +0530

    debugger in js

---
+++ day 12 debugger, variable memory address/script.js
@@ -0,0 +1,14 @@
+// debugger
+// console.log(firstNmae)
+// console.log(lastname)
+// console.log(age)
+
+var firstName = "Akash"
+let lastname = "singh"
+const age = 25
+
+console.log(firstNmae)
+console.log(lastname)
+console.log(age)
+
+let a = "xy"
