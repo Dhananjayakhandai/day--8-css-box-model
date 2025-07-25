4e6419aeff0ae592318c3c5900caffb85bf28dad
Dhananjayakhandai <dhananjayakhandai@gmail.com>
2025-07-25 08:15:03 +0530

    Operators in js

---
+++ day-9 css flexbox/13 trthy and falsy value/operators.js
@@ -0,0 +1,24 @@
+console.log(2 ** 3)
+console.log(3 ** 4)
+
+let a  = 10
+a **= 3
+console.log(a)
+//======================================================================
+
+
+console.log(5 =='5') //('5' coerced -> 5)
+console.log(5 === '5')
+console.log(5 === 5)
+
+console.log(5 !='6') // (no corection)
+console.log(5 !=='6') //true
+console.log(5 !=='5') //false
+
+//=========================================================================
+
+console.log(2 << 3) //2 * 2^3
+console.log(64 << 1) //64 * 2^1
+
+console.log(64 >> 2) //64 /2^2
+console.log(64 >> 1) //64 /2^1
