04547f364b6b271114ee478d9308cc4d093770e4
Dhananjayakhandai <dhananjayakhandai@gmail.com>
2025-07-25 19:40:26 +0530

    positive or negative, even or odd, string is empty or not, character is vowel or constant, eligible to vote or not is completed

---
+++ assignment/positive and negative.js
@@ -0,0 +1,51 @@
+// let num = parseInt(prompt("Enter a number"));
+// if (num > 0) {
+//     console.log("Positive");
+// } else if (num < 0) {
+//     console.log("Negative");
+// } else {
+//     console.log("Zero");
+// }
+
+
+//====================================================
+//Even or odd
+// let num = parseInt(prompt("Enter a number"))
+// if (num %2 ==0) {
+//     console.log("even")
+// }else {
+//     console.log("odd")
+// }
+
+
+//================================================
+//Eligible to vote or not
+
+// let age = parseInt(prompt("Enter your age"))
+// if(age>=18) {
+//     console.log("You can eligible to vote")
+// }else {
+//     console.log("You can't eligible to vote")
+// }
+
+
+//===================================================
+//check the character is a vowel or constant
+
+// let char = prompt("Enter a character")
+// if(char.length !== 1|| !char.match) {
+//     console.log("Not a valid alphabet character")
+// } else if ("aeiou".includes(char)) {
+//     console.log("Vowel")
+// } else {
+//     console.log("Consonant")
+// }
+
+//==================================================
+//check string is empty or not
+let str = prompt("Enter a string:");
+if (str.trim() === "") {
+    console.log("String is empty");
+} else {
+    console.log("String is not empty");
+}
