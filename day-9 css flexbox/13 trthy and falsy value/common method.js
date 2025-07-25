d89557318eb81ffc2a3e6f804643b0b4ed5ea03d
Dhananjayakhandai <dhananjayakhandai@gmail.com>
2025-07-25 07:45:25 +0530

    common methods for interacting  in js

---
+++ day-9 css flexbox/13 trthy and falsy value/common method.js
@@ -0,0 +1,12 @@
+// alert('hii')
+// alert('everyone This plane is going to be crash')
+
+
+// prompt('ate u ok with this ?')
+
+const name = print('Enter your name')
+if(name) {
+    alert('Hello' + username)
+}else {
+    alert('you didnot enter your name')
+}
