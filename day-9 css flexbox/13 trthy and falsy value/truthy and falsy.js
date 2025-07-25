d20eb10384bcf3d1fa9a2198f012eaf72f048652
Dhananjayakhandai <dhananjayakhandai@gmail.com>
2025-07-25 07:43:34 +0530

    truthy and falsy value in js

---
+++ day-9 css flexbox/13 trthy and falsy value/truthy and falsy.js
@@ -0,0 +1,37 @@
+if(2) {
+    console.log("It is a truthy value")
+}else {
+    console.log("It is a falsy value")
+}
+
+if(0) {
+    console.log("It is a truthy value")
+}else {
+    console.log("It is a falsy value")
+}
+
+
+if(undefined) {
+    console.log("It is a truthy value")
+}else {
+    console.log("It is a falsy value")
+}
+
+if(NaN) {
+    console.log("It is a truthy value")
+}else {
+    console.log("It is a falsy value")
+}
+
+if(1) {
+    console.log("It is a truthy value")
+}else {
+    console.log("It is a falsy value")
+}
+
+
+if(-1) {
+    console.log("It is a truthy value")
+}else {
+    console.log("It is a falsy value")
+}
