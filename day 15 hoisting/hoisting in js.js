bf29c78269e882ab01bc042098a506064e89cae5
Dhananjayakhandai <dhananjayakhandai@gmail.com>
2025-07-27 07:35:35 +0530

    hoisting in JavaScript

---
+++ day 15 hoisting/hoisting in js.js
@@ -0,0 +1,11 @@
+console.log(username)
+var username = "Abhishek"
+console.log(username)
+
+console.log(lastname)
+const lastname = "Das"   //let and const variable declare can be store in the script and others are stored in global
+
+
+function sayHii() {
+    console.log("Hello everyone")
+}
