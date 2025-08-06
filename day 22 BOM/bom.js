42da059c951616d8e77dd3c34ccfebfae3e43a2d
Dhananjayakhandai <dhananjayakhandai@gmail.com>
2025-08-06 21:32:52 +0530

    bom is started

---
+++ day 22 BOM/bom.js
@@ -0,0 +1,17 @@
+// setTimeout(location.reload(), 2000)
+// location.href ="index.html"
+
+// history.go()
+// history.back()
+// history.go(1)
+//history.go(-1) = history.back
+
+//innerwidth
+// innerHeight
+
+//outerheight
+// outerWidth
+
+//open()
+
+
